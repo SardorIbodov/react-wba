84d799cc4bdbee70818173c55745dc0de67985dc
SardorIbodov <sardoribodov07@gmail.com>
2023-04-06 18:32:59 +0500

    Intro to React | JSX

---
+++ tutorial1/src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+
+
+// import React from "react";
+// import ReactDOM from "react-dom";
+
+// const title = <h1><em>Hi React</em></h1>; 
+// //JSX
+
+// ReactDOM.render(
+//   title,
+//   document.getElementById("root")
+// );
