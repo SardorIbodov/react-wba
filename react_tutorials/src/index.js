36dc4afcfb8582b01e05d444f06bac7f947f0ad9
SardorIbodov <sardoribodov07@gmail.com>
2023-04-11 11:30:41 +0500

    Updated

--- react_tutorials/src/index.js
+++ react_tutorials/src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Lesson3 from './lesson3';
+import Lesson4 from './lesson4';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-		<Lesson3 />
+		{/* <Lesson3 /> */}
+		{/* <Lesson4 /> */}
   </React.StrictMode>
 );
 
