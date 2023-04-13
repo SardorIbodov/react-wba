9be0f4b51139c1820a90afd546161213d8972210
SardorIbodov <sardoribodov07@gmail.com>
2023-04-13 06:56:41 +0500

    React Lessons

--- react_tutorials/src/index.js
+++ react_tutorials/src/index.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import Lesson3 from './lesson3';
-import Lesson4 from './lesson4';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Lesson3 } from "./lesson3";
+import { Lesson4 } from "./lesson4";
+import { Lesson6 } from "./lesson6";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-		{/* <Lesson3 /> */}
-		{/* <Lesson4 /> */}
+    {/* <Lesson3 /> */}
+    {/* <Lesson4 /> */}
+		<Lesson6 />
   </React.StrictMode>
 );
-
